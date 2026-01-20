d93a402dae108d55d54dd2eb4681fc0cb59e89ac
Emily <emilyahmad77@gmail.com>
2026-01-20 13:16:28 -0500

    no way! it worked

--- Assets/Scripts/player_controller.gd
+++ Assets/Scripts/player_controller.gd
@@ -11,6 +11,18 @@ var direction = 0
 #const SPEED = 300.0
 #const JUMP_VELOCITY = -400.0
 
+#sync project settings gravity with rigid body nodes
+#var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _input(event):
+	if event.is_action_pressed("jump") and is_on_floor():
+		velocity.y = jump_power * jump_multiplier
+	#handle jump down
+	if event.is_action_pressed("move_down"):
+		set_collision_mask_value(10, false)
+	else:
+		set_collision_mask_value(10, true)
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
