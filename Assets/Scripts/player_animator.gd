296403d11076e46e87466007bcf9ab16021fc469
Emily <emilyahmad77@gmail.com>
2026-01-19 19:25:57 -0500

    all up to date

---
+++ Assets/Scripts/player_animator.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+@export var player_controller : PlayerController
+@export var animation_player : AnimationPlayer
+@export var sprite : Sprite2D
+
+#availability to meet with mentors
+#team number = 1
+#handle team adjustments over email rather than google forms
+##permission slips required (each student & parent submit), email pdf/scan
+#find sign ups on page 8&9 of guidebook
+#high school submit proposal (pdf), project poster & presentation
+#explore guidebook for tips on proposal
+# mentors are undergrads
+# can meet 1x1
+#guides are made by post docs (screening newborns for disease, etc.)
+# links to consortiums, publicly available data sets
+# around 100 projects submitted (hackathon level difficulty)
