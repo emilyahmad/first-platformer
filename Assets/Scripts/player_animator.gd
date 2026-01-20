18ecbbb7f720a86a9e209fe5c810006cb2c87eca
Emily <emilyahmad77@gmail.com>
2026-01-19 20:21:58 -0500

    working on tile map

--- Assets/Scripts/player_animator.gd
+++ Assets/Scripts/player_animator.gd
@@ -4,15 +4,19 @@ extends Node2D
 @export var animation_player : AnimationPlayer
 @export var sprite : Sprite2D
 
-#availability to meet with mentors
-#team number = 1
-#handle team adjustments over email rather than google forms
-##permission slips required (each student & parent submit), email pdf/scan
-#find sign ups on page 8&9 of guidebook
-#high school submit proposal (pdf), project poster & presentation
-#explore guidebook for tips on proposal
-# mentors are undergrads
-# can meet 1x1
-#guides are made by post docs (screening newborns for disease, etc.)
-# links to consortiums, publicly available data sets
-# around 100 projects submitted (hackathon level difficulty)
+func _process(delta):
+	# flip based on player's input direction
+	if player_controller.direction == 1:
+		sprite.flip_h = false
+	elif player_controller.direction == -1:
+		sprite.flip_h = true
+	
+	if abs(player_controller.velocity.x) > 0.0:
+		animation_player.play("move")
+	else:
+		animation_player.play("idle")
+	
+	if player_controller.velocity.y < 0.0:
+		animation_player.play("jump")
+	elif player_controller.velocity.y > 0.0:
+		animation_player.play("fall")
